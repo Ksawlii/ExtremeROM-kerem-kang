0178eb56480a03a7eb3bc43f973d0f2fcc289b5a
Kerem_38 <keremkaan2712@gmail.com>
2025-06-12 13:26:36 +0300

    fix sound

---
+++ target/a50/patches/stock_blobs/customize.sh
@@ -0,0 +1,17 @@
+echo "- Add stock vintf manifest"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/vintf/compatibility_matrix.device.xml" 0 0 644 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/vintf/manifest.xml" 0 0 644 "u:object_r:system_file:s0"
+
+echo "- Fix sound quality"
+DELETE_FROM_WORK_DIR "system" "system/etc/stage_policy.conf"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_soundaliveresampler.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2060.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/stage_policy.conf" 0 0 644 "u:object_r:system_file:s0"
+
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/lib_SoundBooster_ver1000.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1000.so" 0 0 644 "u:object_r:system_lib_file:s0"
+A
+
