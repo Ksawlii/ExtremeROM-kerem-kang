24bc1bbcc4be5dd3ba769bb069a36e3a1c757566
Kerem_38 <keremkaan2712@gmail.com>
2025-06-24 11:37:47 +0300

    Update customize.sh

--- platform/exynos9610/patches/camera/customize.sh
+++ platform/exynos9610/patches/camera/customize.sh
@@ -18,16 +18,6 @@ while IFS= read -r blob; do
     [ -n "$blob" ] && ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/$blob" 0 0 644 "u:object_r:system_lib_file:s0"
 done < "$BLOBS_LIST"
 
-echo "- Fix AI Photo Editor"
-cp -a --preserve=all \
-    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json" \
-    "$WORK_DIR/system/system/cameradata/portrait_data/nexus_bokeh_feature.json"
-SET_METADATA "system" "system/cameradata/portrait_data/nexus_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
-sed -i "s/MODEL_TYPE_INSTANCE_CAPTURE/MODEL_TYPE_OBJ_INSTANCE_CAPTURE/g" \
-    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json"
-sed -i \
-    's/system\/cameradata\/portrait_data\/single_bokeh_feature.json/system\/cameradata\/portrait_data\/nexus_bokeh_feature.json\x00/g' \
-    "$WORK_DIR/system/system/lib64/libPortraitSolution.camera.samsung.so"
 
 echo "- Fix changing wallpapers"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libImageTagger.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
